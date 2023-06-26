01a3aed5d3627ffe0451395cba42eab71b42089f
ahvino <seliksamai@gmail.com>
2023-06-25 18:26:03 -0700

    face rec project

--- FacialRecognition/FaceRec.py
+++ FacialRecognition/FaceRec.py
@@ -8,5 +8,3 @@ face_analysis = DeepFace.analyze(img_path="FacialRecognition/happy_face.jpeg")
 print(face_analysis[0]["emotion"])
 
 
-  git config --global user.email "seliksamai@gmail.com"
-  git config --global user.name "ahvino"
