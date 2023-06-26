8b544cddb065a5ee1bc41906fdfb8d94d06c2dc2
avino <seliksamai@gmail.com>
2023-06-25 21:45:19 -0700

    added link

--- Sentiment Analysis/main.py
+++ Sentiment Analysis/main.py
@@ -1,7 +1,7 @@
 import pyfacebook
 import fb
 import facebook
-
+# https://developers.facebook.com/tools/explorer/
 appID = 1313828222853030
 token = 'EAASq63iE36YBAPI8MQdPuYKnUNGkUokv1AUoJSYBb2fcCYSz8wX3q5DiYqeYyZAwlcgh66JJxZAqGs4iyyuj6ZC1zfYGAzUhid8IQQpZC4ZAoZBZC3Vbf4TzBVT18smBjKDd2SrJ565NVx3edgnuVUtstOBfynHYkYsr4hwpNciT9d6u9EtAZAdWWXGhAv1eDad8a8l7DyQGA0FiYLAzayMqdv01WWypMIFnSr8CF0SWuGLvAdvRTuNJ'
 appToken = '1313828222853030|rYIDd8lwhcnLgM38PDwoF_YiFko'
